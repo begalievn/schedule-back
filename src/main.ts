c21b27f25cbcbdd89e94aea45d3350ad79d93750
begalievn <begaliev.info@gmail.com>
2023-03-06 12:31:37 +0600

    Deleted .env file

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
   app.enableCors();
-  await app.listen(PORT, () => console.log(`Application has started on port ${PORT}`));
+  await app.listen(PORT, () =>
+    console.log(`Application has started on port ${PORT}`),
+  );
 }
 
 bootstrap();
