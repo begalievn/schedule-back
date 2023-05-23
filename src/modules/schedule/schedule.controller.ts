64b157253d5de5331cef8323f49574b37cc8a6da
begalievn <begaliev.info@gmail.com>
2023-05-23 16:15:34 +0600

    feat: Added updating and removing schedule

--- src/modules/schedule/schedule.controller.ts
+++ src/modules/schedule/schedule.controller.ts
@@ -1,8 +1,18 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { ScheduleService } from './schedule.service';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ListScheduleParamsDto } from './dto/list-schedule-params.dto';
 import { CreateScheduleDto } from './dto/create-schedule.dto';
+import { UpdateScheduleDto } from './dto/update-schedule.dto';
 
 @ApiTags('Schedule')
 @Controller('schedule')
@@ -31,5 +41,20 @@ export class ScheduleController {
   @ApiOperation({ summary: 'List of subjects to create a schedule' })
   findAll(@Query() listParams: ListScheduleParamsDto) {
     return this.scheduleService.getExampleSubjects(listParams);
+  }
+
+  @Patch(':id')
+  @ApiOperation({ summary: 'Update a schedule by id' })
+  async update(
+    @Param('id') id: string,
+    @Body() updateScheduleDto: UpdateScheduleDto,
+  ) {
+    return this.scheduleService.update(id, updateScheduleDto);
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: 'Delete a schedule by id' })
+  async remove(@Param('id') id: string) {
+    return this.scheduleService.delete(id);
   }
 }
